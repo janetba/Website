2c1af66c63ee63952fd11bed4b70598c560c952f
janetba <janetba@outlook.com>
2015-07-27 00:11:45 -0700

    s

--- src/app.php
+++ src/app.php
@@ -50,7 +50,7 @@ $app->match('/add', function (Request $request) use ($app) {
                 throw new \InvalidArgumentException('The uploaded photo file is not valid.');
             }
             // Upload the photo to S3
-			echo '<script type="text/javascript">alert("'$file->getClientOriginalName()'")</script>';
+			echo "$file->getClientOriginalName()";
 
             $key = time() . '-' . strtolower(str_replace(array(' ', '_', '/'), '-', $file->getClientOriginalName()));
             $app['aws']->get('s3')->putObject(array(
