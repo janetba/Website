a193174bd0fb283a24f2d4569f0c3567ec7ba15c
janetba <janetba@outlook.com>
2015-07-27 00:38:17 -0700

    adfs

--- src/app.php
+++ src/app.php
@@ -46,22 +46,12 @@ $app->match('/add', function (Request $request) use ($app) {
         try {
             // Make sure the photo was uploaded without error
             $file = $request->files->get('photoFile');
-            if (!$file instanceof UploadedFile || $file->getError()) {
+            if (!$file instanceof UploadedFile || $file->getError() || !($file->getExtension() <=> 'jpg')) {
                 throw new \InvalidArgumentException('The uploaded photo file is not valid.');
             }
             // Upload the photo to S3
-			
-			if($file->getClientOriginalName() != null) {
-				echo "$file->getClientOriginalName()";
-			}
-			else{
-				echo "empty";
-			}
-				
-			
-		
             $key = time() . '-' . strtolower(str_replace(array(' ', '_', '/'), '-', $file->getClientOriginalName()));
-			echo "$key"
+			
             $app['aws']->get('s3')->putObject(array(
                 'Bucket' => $app['aws.bucket'],
                 'Key'    => $key,
